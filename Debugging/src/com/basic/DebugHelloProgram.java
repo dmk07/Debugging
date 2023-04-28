902cb5279cf6de2abc24bd4435eae53baf7fced5
dmk61 <dmk61@Dipak-Kadam>
2023-04-29 01:40:29 +0530

    first debugging program

---
+++ Debugging/src/com/basic/DebugHelloProgram.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class DebugHelloProgram {
+
+	public static void main(String[] args) {
+
+		System.out.print("Hello I am debugging you...");
+	
+		new DebugHelloProgram();
+		
+
+	}
+	
+	
+	
+
+}

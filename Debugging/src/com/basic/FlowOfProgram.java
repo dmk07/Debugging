426b0f3e435bca510cf110f93d13a1c6b4957d80
dmk61 <dmk61@Dipak-Kadam>
2023-04-29 02:02:25 +0530

    method debugging flow

---
+++ Debugging/src/com/basic/FlowOfProgram.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+public class FlowOfProgram {
+
+	public static void show()
+	{
+		System.out.print("show method");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+	
+		
+		System.out.print("show method");
+		
+		show();
+		
+		
+		
+
+	}
+
+}
